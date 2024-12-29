79244141a16588220acea63c4284dc3ddce7adfc
venturas <venturas55@hotmail.com>
2024-12-29 11:49:08 +0100

    Perfil funcionando

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -3,30 +3,42 @@ import React from "react";
 import BeltComponent from "./BeltComponent";
 import useUsuario from "../hooks/useUsuario";
 
-const Perfil = (user) => {
-  const usuario = useUsuario(1); // Estado para almacenar las clases
+const Perfil = ({ user }) => {
+  const usuario = useUsuario(1);
+  console.log(usuario);
 
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={{
-          uri: "http://adriandeharo.es:7001/api/usuario/foto/" + usuario.id,
-        }}
-      />
-      <Text>{usuario.nombre}</Text>
-      <Text>{usuario.apellidos}</Text>
-      <Text>{usuario.fecha_nacimiento}</Text>
-      <Text>{usuario.pais}</Text>
-      <Text>{usuario.telefono}</Text>
-      <Text>{usuario.cinturon}</Text>
-      <Text>{usuario.grado}</Text>
-      <BeltComponent
-        cinturon={usuario.cinturon}
-        grados={usuario.grado}
-        id={usuario.id}
-        tamano="pequeño"
-      />
+      {usuario ? (
+        <>
+          <Image
+            style={styles.image}
+            source={{
+              uri: "http://adriandeharo.es:7001/api/usuario/foto/" + usuario.id,
+            }}
+          />
+          <Text>{usuario.nombre}</Text>
+          <Text>{usuario.apellidos}</Text>
+          <Text>{usuario.pais}</Text>
+          <Text>{usuario.fecha_nacimiento}</Text>
+          <Text>{usuario.pais}</Text>
+          <Text>{usuario.genero}</Text>
+          <Text>{usuario.codigo_iso}</Text>
+          <Text>
+            {usuario.pais_telefono} {usuario.telefono}
+          </Text>
+          <Text>{usuario.cinturon}</Text>
+          <Text>{usuario.grado}</Text>
+          <BeltComponent
+            cinturon={usuario.cinturon}
+            grados={usuario.grado}
+            id={usuario.id}
+            tamano="pequeño"
+          />
+        </>
+      ) : (
+        <Text>Recibiendo datos</Text>
+      )}
     </View>
   );
 };
